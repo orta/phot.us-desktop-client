5894cde4a86674e2e42c826825f3746537551a84
orta <orta.therox@gmail.com>
2011-03-26 21:51:58 +0000

    selecting in  the browserview now sets the imageview

--- photouploader/AppDelegate.rb
+++ photouploader/AppDelegate.rb
@@ -7,21 +7,27 @@
 #
 
 class AppDelegate
-  attr_accessor :window, :imageView
+  attr_accessor :window, :imageBrowserView, :imageView
   
   def applicationDidFinishLaunching(a_notification)
     @images = NSMutableArray.array
   end
   
   def application(sender, openFiles:files)
+    add_files file
+  end
+  
+  def add_files files
     files.each do |file_path|
       photo = Photo.new
       photo.imageUID = file_path
       photo.imageTitle = file_path
       @images << photo
     end
-    imageView.reloadData
+    imageBrowserView.reloadData
   end
+  
+  #stuff for IKBrowser
   
   def numberOfItemsInImageBrowser(view)
     @images.count
@@ -33,9 +39,20 @@ class AppDelegate
   
   def imageBrowser(aBrowser, removeItemsAtIndexes:indexes)
     @images.removeObjectsAtIndexes indexes
-    imageView.reloadData
+    imageBrowserView.reloadData
+  end  
+  
+  def imageBrowserSelectionDidChange(browser)
+    index = browser.selectionIndexes.firstIndex
+    url = NSURL.fileURLWithPath @images[index].imageUID
+    
+    imageView.setImageWithURL url
   end
 
+  #stuff for imageView
+  def selectionRectAdded(imageView)
+    puts "OK"
+  end
   
 end
 
